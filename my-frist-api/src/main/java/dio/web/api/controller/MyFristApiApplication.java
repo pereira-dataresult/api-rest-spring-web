4ad2d7d8d24a2e67a2ca283bbf1a78a2fb441c0a
Yagoo Pereira <yagoosjn@gmail.com>
2023-04-16 13:17:06 -0300

    subindo arquivos

---
+++ my-frist-api/src/main/java/dio/web/api/controller/MyFristApiApplication.java
@@ -0,0 +1,13 @@
+package dio.web.api.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFristApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFristApiApplication.class, args);
+	}
+
+}
